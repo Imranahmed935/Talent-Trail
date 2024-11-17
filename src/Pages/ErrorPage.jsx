d82f2ead893b7454e9e9bf4d9c23401f6f77b645
Imranahmed935 <imrantahir9918@gmail.com>
2024-11-17 10:08:20 -0800

    all technology installed and navbar setup done

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='text-center'>
+            <h1 className='text-4xl text-red-600'>Ops, Page Not Found.</h1>
+            <h2 className='text-2xl font-bold'>404 Error</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+           <nav className='lg:w-9/12 mx-auto py-6'>
+             <Navbar/>
+           </nav>
+        </div>
+    );
+};
+
+export default Home;
