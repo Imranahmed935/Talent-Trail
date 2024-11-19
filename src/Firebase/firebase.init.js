139cc9239a6dbccb1f016aa9e3fe64d8c813c71a
Imranahmed935 <imrantahir9918@gmail.com>
2024-11-19 09:00:45 -0800

    environment variables setup

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5n1s17yWbxLC_M0RYT9dlS_JjkzqT_Rk",
-  authDomain: "talent-trail-e14ba.firebaseapp.com",
-  projectId: "talent-trail-e14ba",
-  storageBucket: "talent-trail-e14ba.firebasestorage.app",
-  messagingSenderId: "158028049902",
-  appId: "1:158028049902:web:6583711440b80cda2ff2ff"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
